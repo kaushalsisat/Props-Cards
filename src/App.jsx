ac9d3c29a66ecde422d86a5c16ee707ef19e30c4
kaushalsisat@gmail.com <kaushalsisat@gmail.com>
2025-01-02 18:53:00 +0530

    by ks

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,10 +12,10 @@ function App() {
     <h1 className="text-3xl font-bold underline text-gray-600 shadow-xl">
        Create Cards Using Props
     </h1>
-     <div className='m-8'><Card username="Om" img="/src/assets/images/i.jpg" /> </div>
-     <div className='m-8'> <Card username="Karan" img="/src/assets/images/ii.jpg" /> </div>
-     <div className='m-8'> <Card username="Vivek" img="/src/assets/images/iii.jpg" /> </div>
-     <div className='m-8'> <Card username="Kaushal" img="/src/assets/images/iv.jpg" /> </div>
+     <div className='m-8'><Card username="Om" img="./src/assets/images/i.jpg" /> </div>
+     <div className='m-8'> <Card username="Karan" img="./src/assets/images/ii.jpg" /> </div>
+     <div className='m-8'> <Card username="Vivek" img="./src/assets/images/iii.jpg" /> </div>
+     <div className='m-8'> <Card username="Kaushal" img="./src/assets/images/iv.jpg" /> </div>
      <div className='m-8'> <Card /> </div>
     </>
   )
